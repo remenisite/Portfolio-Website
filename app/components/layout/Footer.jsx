c309e6867a99646c8281c3b2d33ef3c0c6dfb69f
antor94 <antorremeni@gmail.com>
2026-02-01 22:18:15 +0600

    final code

--- app/components/layout/Footer.jsx
+++ app/components/layout/Footer.jsx
@@ -1,8 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='bg-[#1e1e1e] pt-[54px] pb-[34px]'>
+      <div className="container flex justify-between items-center">
+
+        <h2 className='text-[20px] font-semibold font-main text-white'>Copyright @2026,<span className='font-kalam text-text'> remeni</span> All Rights Reserved</h2>
+
+                <ul className="hidden md:flex space-x-6 text-[18px] font-semibold font-main text-[#ffffffa6]">
+
+          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> About</Link></li>
+          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Services</Link></li>
+          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Resume</Link></li>
+          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Skills</Link></li>
+          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Projects</Link></li>
+        </ul>
+
+      </div>
+    </section>
   )
 }
 
