5ba30e92e712ae3b1b3d35987f9f1f333215eb5e
antor94 <antorremeni@gmail.com>
2026-02-04 14:20:57 +0600

    change code

--- app/components/contact/ContactSection.jsx
+++ app/components/contact/ContactSection.jsx
@@ -5,7 +5,7 @@ import LeftSection from "./LeftSection";
 
 const ContactSection = () => {
   return (
-    <section data-aos="fade-up" className=" pt-[57px] pb-[90px]">
+    <section id="contact" data-aos="fade-up" className=" pt-[57px] pb-[90px]">
       <div className="container  grid grid-cols-2">
         <LeftSection />
         <ContactForm />
--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -13,18 +13,18 @@ useEffect(() => { Aos.init({ duration: 1000,}) }, [])
       <nav className="container flex justify-between items-center">
         {/* Logo */}
         <div>
-            <Link data-aos="flip-right" href={"#"} ><Image src="/iamges/logo.png" width={60} height={0} alt="logo" /></Link>
+            <Link data-aos="flip-right" href={"/"} ><Image src="/iamges/logo.png" width={60} height={0} alt="logo" /></Link>
         </div>
 
         {/* Menu */}
         <ul data-aos="zoom-in" className="hidden md:flex space-x-6 text-[24px] font-semibold font-main text-[#ffffffa6]">
-          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Home</Link></li>
+          <li><Link href="/" className="flex items-center gap-1 hover:text-text duration-300"> Home</Link></li>
           <li><Link href="#about" className="flex items-center gap-1 hover:text-text duration-300"> About</Link></li>
           <li><Link href="#service" className="flex items-center gap-1 hover:text-text duration-300"> Services</Link></li>
           <li><Link href="#resume" className="flex items-center gap-1 hover:text-text duration-300"> Resume</Link></li>
           <li><Link href="#skill" className="flex items-center gap-1 hover:text-text duration-300"> Skills</Link></li>
           <li><Link href="#project" className="flex items-center gap-1 hover:text-text duration-300"> Projects</Link></li>
-          <li><Link href="#" className="flex items-center gap-1 hover:text-text duration-300"> Contact</Link></li>
+          <li><Link href="#contact" className="flex items-center gap-1 hover:text-text duration-300"> Contact</Link></li>
         </ul>
 
         {/* Hire Me Button */}
